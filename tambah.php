298a24e6ad08daab87820e2a7724ad357edcac77
zulkamaula <mzulkariski@gmail.com>
2020-11-27 13:07:42 +0700

    Mengedit file style.css dan menambahkan style pada halaman Ubah

--- tambah.php
+++ tambah.php
@@ -2,7 +2,7 @@
 
 session_start();
 // cek session
-if( !isset($_SESSION["login"]) ){
+if (!isset($_SESSION["login"])) {
     header("Location: login.php");
     exit;
 }
@@ -11,14 +11,14 @@ if( !isset($_SESSION["login"]) ){
 require 'functions.php';
 
 // cek apakah tombol submit sudah di tekan atau belum
-if( isset($_POST["submit"]) ){
+if (isset($_POST["submit"])) {
 
     // ambil data dari tiap elemen form yg disimpan di functions.php
 
     // query insert data di simpan dalam function.php 
-    
+
     // mengecek apakah data berhasil ditambahkan atau tidak dg sintak di bawah ini
-    if( tambah($_POST) > 0 ){
+    if (tambah($_POST) > 0) {
         echo "
         // memunculka alert dg javascript untuk berpindah halaman.
 
@@ -40,51 +40,51 @@ if( isset($_POST["submit"]) ){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Data Mahasiswa</title>
-    <style>
-    ul li {
-        list-style: none;
-        margin: 10px auto;
-    }
-    </style>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <h1>Tambah Data Mahasiswa</h1>
-    <form action="" method="post" enctype="multipart/form-data">
-        <ul>
-            <li>
-                <label for="npm">NPM :</label>
-                <input type="text" name="npm" id="npm" required>
-            </li>
-            
-            <li>
-                <label for="nama">Nama :</label>
-                <input type="text" name="nama" id="nama" required>
-            </li>
+    <div class="contianer-tambah">
+        <h1>Tambah Data Mahasiswa</h1>
+        <form action="" method="post" enctype="multipart/form-data">
+            <ul>
+                <li>
+                    <label for="npm">NPM :</label>
+                    <input type="text" name="npm" id="npm" required>
+                </li>
 
-            <li>
-                <label for="email">E-mail :</label>
-                <input type="text" name="email" id="email" required>
-            </li>
+                <li>
+                    <label for="nama">Nama :</label>
+                    <input type="text" name="nama" id="nama" required>
+                </li>
 
-            <li>
-                <label for="jurusan">Jurusan :</label>
-                <input type="text" name="jurusan" id="jurusan" required>
-            </li>
+                <li>
+                    <label for="email">E-mail :</label>
+                    <input type="text" name="email" id="email" required>
+                </li>
 
-            <li>
-                <label for="gambar">Gambar :</label>
-                <input type="file" name="gambar" id="gambar">
-            </li>
+                <li>
+                    <label for="jurusan">Jurusan :</label>
+                    <input type="text" name="jurusan" id="jurusan" required>
+                </li>
 
-            <li>
-                <button type="submit" name="submit">Tambah Data</button>
-            </li>
+                <li>
+                    <label for="gambar">Gambar :</label>
+                    <input type="file" name="gambar" id="gambar">
+                </li>
 
-        </ul>
+                <li>
+                    <button type="submit" name="submit">Tambah Data</button>
+                </li>
+
+            </ul>
         </form>
+    </div>
 </body>
+
 </html>
